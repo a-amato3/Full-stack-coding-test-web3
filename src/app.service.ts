9c8a14119cd82c256f99c2436fc183f588f0531d
a-amato93 <aaron@downforce.tech>
2022-05-05 15:09:09 +0100

    baseic get request on local adresses

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import * as _ from 'lodash';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const Web3 = require('web3');
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  public async getAddress(): Promise<void> {
+    const web3 = new Web3(
+      new Web3.providers.HttpProvider('http://localhost:7545'),
+    );
+
+    if (!_.isUndefined(web3)) {
+      // Get address
+      const accounts = await web3.eth.getAccounts();
+      console.dir(accounts);
+
+      // Get balance
+      const balance = await web3.eth.getBalance(accounts[0]);
+      console.log(`balance : ${balance}`);
+
+      /* // Get fromWei
+      web3.eth.getBalance(accounts[0], async (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          const wei = await web3.utils.fromWei(result, 'ether');
+          console.log(wei + ' ETH');
+        }
+      }); */
+    }
+    return null;
   }
 }
