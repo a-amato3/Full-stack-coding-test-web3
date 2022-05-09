464535ed89c4a27189b1e07e2596cac6d954a829
a-amato93 <aaron@downforce.tech>
2022-05-09 17:04:48 +0100

    added truffle abi

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import * as _ from 'lodash';
 import { ERC20 } from './models/token-interface';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const abi = require('./assets/abi.json');
+const abiContract = require('../build/contracts/migrations.json');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const Web3 = require('web3');
 
@@ -12,41 +12,47 @@ export class AppService {
   public async getAddress(): Promise<void> {
     if (!_.isUndefined(web3)) {
       const accounts = await web3.eth.getAccounts();
-      accounts.forEach((account: any): void => {
-        this.getAccountDetails(account);
+      accounts.forEach((account: string): void => {
+        this.getAccountDetails(account).then((results: ERC20): void => {
+          console.log(results);
+        });
       });
     }
     return null;
   }
 
-  private async getAccountDetails(account: any): Promise<void> {
+  private async getAccountDetails(account: string): Promise<ERC20> {
     const tokenAddress = '0x394684c6fdfEDC39ABF7BAf1c482477E8E795b62'; // PASTE TOKEN ADDRESS HERE
 
-    const tokenContract = new web3.eth.Contract(abi, tokenAddress);
-    this.getTokenInfo(tokenContract);
+    const tokenContract = await new web3.eth.Contract(
+      [abiContract],
+      tokenAddress,
+    );
+    const getToken = async (): Promise<any> => {
+      return await this.getTokenInfo(tokenContract);
+    };
 
     const getBalance = async (): Promise<void> => {
       return await web3.eth.getBalance(account);
     };
-    getBalance().then((balanceWallet): void => {
-      const address = [];
-      address.push({
-        token_address: account,
-        name: 'Ethereum',
-        symbol: 'ETH',
-        decimals: 8,
-        balance: balanceWallet,
-        balance_usd: '2,698.17',
-      } as ERC20);
-      console.log(address);
-    });
+
+    const token = await getToken();
+    const balance = await getBalance();
+    return {
+      token_address: account,
+      name: token.name,
+      symbol: token.symbol,
+      decimals: token.decimals,
+      balance,
+      balance_usd: '2,380.60',
+    } as ERC20;
   }
 
-  private async getTokenInfo(tokenContract) {
-    const [decimals, name, symbol] = await Promise.all([
+  private async getTokenInfo(tokenContract): Promise<any> {
+    const [name, symbol, decimals] = await Promise.all([
+      tokenContract.methods.name().call(),
       tokenContract.methods.symbol().call(),
       tokenContract.methods.decimals().call(),
-      tokenContract.methods.name().call(),
     ]);
     return { decimals, name, symbol };
   }
