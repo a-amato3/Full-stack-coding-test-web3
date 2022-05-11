0237048c5208ed64b8afe5eb17b7f7cf8181eeb1
a-amato93 <aaron@downforce.tech>
2022-05-11 22:15:01 +0100

    fix for JSON format

--- src/app.service.ts
+++ src/app.service.ts
@@ -11,18 +11,14 @@ const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));
 export class AppService {
   public async getAddress(): Promise<ERC20> {
     if (!_.isUndefined(web3)) {
-      const accounts = await web3.eth.getAccounts();
-      accounts.forEach((account: string): void => {
-        this.getAccountDetails(account).then((results: ERC20): void => {
-          // Print JSON response
-          console.log(results);
-        });
+      this.getAccountDetails().then((result: ERC20[]): void => {
+        console.log(result);
       });
     }
     return null;
   }
 
-  private async getAccountDetails(account: string): Promise<ERC20> {
+  private async getAccountDetails(): Promise<ERC20[]> {
     const tokenAddress = '0x394684c6fdfEDC39ABF7BAf1c482477E8E795b62'; // PASTE TOKEN ADDRESS HERE
 
     const tokenContract = await new web3.eth.Contract(
@@ -33,20 +29,27 @@ export class AppService {
       return await this.getTokenInfo(tokenContract);
     };
 
-    const getBalance = async (): Promise<any> => {
-      return await web3.eth.getBalance(account);
-    };
+    const accounts: [] = await web3.eth.getAccounts();
 
-    const token = await getToken();
-    const balance = await getBalance();
-    return {
-      token_address: account,
-      name: token.name,
-      symbol: token.symbol,
-      decimals: token.decimals,
-      balance,
-      balance_usd: balance * 2371,
-    } as ERC20;
+    const tokenArray: ERC20[] = [];
+
+    for (const account of accounts) {
+      const getBalance = async (): Promise<any> => {
+        return await web3.eth.getBalance(account);
+      };
+      const token = await getToken();
+      const balance = await getBalance();
+
+      tokenArray.push({
+        token_address: account,
+        name: token.name,
+        symbol: token.symbol,
+        decimals: token.decimals,
+        balance,
+        balance_usd: balance * 2371,
+      } as ERC20);
+    }
+    return tokenArray;
   }
 
   private async getTokenInfo(tokenContract): Promise<any> {
